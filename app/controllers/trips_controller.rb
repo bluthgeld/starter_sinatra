51ccb9d5bdd0700829bd460fd1204ad814ef70d0
Robert Pedersen <rob@worldcrimeleague.org>
2019-06-06 12:20:46 -0400

    trips edit and all shows

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -35,7 +35,31 @@ class TripsController < Sinatra::Base
   end
 
 
+  delete "/trips/:id" do
+    id = params[:id]
+    @trip = Trip.find(id)
+    @trip.destroy
+    redirect "/trips"
+  end
 
+  get "/trips/:id/edit" do
+    id = params[:id]
+    @trip = Trip.find(id)
+    erb :edit
+  end
+
+  put "/trips/:id" do
+    id = params[:id]
+    @trip = Trip.find(id)
+    date = params["date"]
+    mileage = params["mileage"]
+    pickup = params["pick_up_loc"]
+    dropoff = params["drop_off_loc"]
+    rider = params["rider_id"]
+    scooter = params["scooter_id"]
+    @trip.update(date: date , mileage: mileage , pick_up_loc: pickup , drop_off_loc: dropoff , rider_id: rider , scooter_id: scooter)
+    redirect "trips/#{@trip.id}"
+  end
 
 
 end
